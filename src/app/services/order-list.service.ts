f1145185e98f79cad946b29f7c131ee9c8ec450a
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2025-09-01 11:40:11 +0300

    dalia

--- src/app/services/order-list.service.ts
+++ src/app/services/order-list.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
-import { baseUrl } from '../environment'; 
+import { baseUrl } from '../environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,17 @@ export class OrderListService {
 
   private apiUrl = `${baseUrl}api`;
     constructor(private http: HttpClient, private authService: AuthService) {}
-  
+
     getOrdersList(): Observable<any> {
       // const token = this.authService.getToken();
       const token = localStorage.getItem('authToken');
-  
+
       if (!token) {
         throw new Error('No authentication token found');
       }
-  
+
       const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  
+
       return this.http.get(`${this.apiUrl}/orders/list`, { headers });
     }
 }
