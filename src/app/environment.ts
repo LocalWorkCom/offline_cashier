5fb11bb950bcc84a7a6b12e5b92aea3dffe4fa61
omnia <omnia@ot.com.sa>
2025-09-15 20:55:43 +0300

    change url

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,9 +1,9 @@
 // export const baseUrl='https://erpsystem.testdomain100.online/'
   //export const baseUrl='https://productowner.testdomain100.online/'
 
-export const baseUrl='http://127.0.0.1:8000/'
+//export const baseUrl='http://127.0.0.1:8000/'
 
-// export const baseUrl='https://offline.testdomain100.online/'
+ export const baseUrl='https://offline.testdomain100.online/'
 
 
 export const environment = {
