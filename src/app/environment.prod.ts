642771a81cb885a0181f2ee16a3c599d3eb74c5d
Alaa-Mustafaa <alaamustafaelsayedd@gmail.com>
2025-11-11 16:54:59 +0200

    env

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,6 +1,10 @@
   
 
 
+ export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
+
+/*  export const baseUrl ='http://127.0.0.1:8000/'
+ */ export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
 //for test domain  
 // export const baseUrl='https://erpsystem.testdomain100.online/'
 // export const environment = {    
@@ -37,3 +41,19 @@ export const baseUrl='https://productowner.testdomain100.online/'
      cluster: 'mt1',
    }
  }; 
+
+
+
+
+
+ 
+export const environment2 = {
+  production: false,
+  pusher: {
+    key: 'localkey', 
+    wsHost: 'erpfortest.testdomain100.online',
+    wsPort: 6001,
+    cluster: 'mt1',
+    forceTLS: false,
+  },
+};
