9c940254e2e291635f2e33ab9200f7b2bc0f622f
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2025-12-09 11:39:07 +0200

    dalia print test last version

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -12,10 +12,11 @@ export const environment = {
     cluster: 'mt1',
   }
 };
+
 export const environment2 = {
   production: false,
   pusher: {
-    key: 'localkey', 
+    key: 'localkey',
     wsHost: 'erpfortest.testdomain100.online',
     wsPort: 6001,
     cluster: 'mt1',
