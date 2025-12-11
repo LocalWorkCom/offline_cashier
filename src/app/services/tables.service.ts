41c19f7bc77ab028e1c24a11b028880eb597d464
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2025-12-11 15:44:27 +0200

    table Transfer

--- src/app/services/tables.service.ts
+++ src/app/services/tables.service.ts
@@ -32,6 +32,18 @@ export class TablesService {
     };
     return this.http.post(`${this.apiUrl}/orders/tableorderDetails`, body, { headers });
   }
+  // UPDATE TABLE STATUS
+  updateTableStatus(tableId: number, orderId: number): Observable<any> {
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.token}`
+    );
+    const body = {
+      table_id: tableId,
+      order_id: orderId
+    };
+    return this.http.post(`${this.apiUrl}/orders/order-change-table`, body, { headers });
+  }
   //start dalia
   fetchAndSave(): Observable<any> {
     return new Observable(observer => {
