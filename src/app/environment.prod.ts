de1611e4e9aabd226c08babba0921cb861110a02
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2025-12-21 16:37:46 +0200

    print kitchen from waiter

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,13 +1,13 @@
-  
+
 
 
  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
 /*  export const baseUrl ='http://127.0.0.1:8000/'
- */ export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
-//for test domain  
+ */ export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
+//for test domain
 // export const baseUrl='https://erpsystem.testdomain100.online/'
-// export const environment = {    
+// export const environment = {
 //   production: true,
 //   pusher: {
 //     key: 'cfd52a74b92f9e278f2d',
@@ -16,14 +16,14 @@
 // };
 
 
-//alkoot 
+//alkoot
 
 
 // export const baseUrl='https://alkoot-restaurant.com/'
-//  export const environment = {  
+//  export const environment = {
 //    production: true,
 //    pusher: {
-//      key: '77f608d73899bd256cfa',  
+//      key: '77f608d73899bd256cfa',
 //      cluster: 'mt1',
 //    }
 //  };
@@ -34,21 +34,21 @@
 
 
 // export const baseUrl='https://productowner.testdomain100.online/'
-export const baseUrl='https://erpsystem.testdomain100.online/'
+export const baseUrl='https://erp-cashier.testdomain100.online/'
 
 
- export const environment = {  
+ export const environment = {
    production: true,
    pusher: {
-     key: '77f608d73899bd256cfa',  
+     key: 'cfd52a74b92f9e278f2d',
      cluster: 'mt1',
    }
- }; 
- 
+ };
+
 export const environment2 = {
   production: true,
   pusher: {
-    key: 'localkey', 
+    key: 'localkey',
     wsHost: 'erpfortest.testdomain100.online',
     wsPort: 6001,
     cluster: 'mt1',
