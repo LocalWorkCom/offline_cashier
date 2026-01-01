73d7520c7e5192b6468785f279de71e111dbe4ec
hHanan13 <34395039+hHanan13@users.noreply.github.com>
2026-01-01 10:53:42 +0200

    hanan

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -4,7 +4,9 @@
  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
 /*  export const baseUrl ='http://127.0.0.1:8000/'
- */ export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
+ */ 
+//export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
+export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
 //for test domain
 // export const baseUrl='https://erpsystem.testdomain100.online/'
 // export const environment = {
