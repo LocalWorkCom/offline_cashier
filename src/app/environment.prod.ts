c6b7f601184e186c7956b13d18af66704d307513
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2026-01-02 23:00:30 +0200

    dd

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -6,7 +6,8 @@
 /*  export const baseUrl ='http://127.0.0.1:8000/'
  */
 //export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
-export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
+// export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
+export const baseUrl2 ='https://productowner.testdomain100.online/api'
 //for test domain
 // export const baseUrl='https://erpsystem.testdomain100.online/'
 // export const environment = {
@@ -35,9 +36,9 @@ export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
 //production
 
 
-// export const baseUrl='https://productowner.testdomain100.online/'
+export const baseUrl='https://productowner.testdomain100.online/'
 // export const baseUrl='https://erpsystem.testdomain100.online/'
-export const baseUrl='https://erp-cashier.testdomain100.online/'
+// export const baseUrl='https://erp-cashier.testdomain100.online/'
 
 
  export const environment = {
