0c1d06ff0842a43c1a77314e604a8d172661413a
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2026-01-06 01:23:41 +0200

    s

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -4,9 +4,9 @@
  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
 /*  export const baseUrl ='http://127.0.0.1:8000/'
- */ 
+ */
 //export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
-export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
+export const baseUrl2 ='https://erpmain.alkoot-restaurant.com/api'
 //for test domain
 // export const baseUrl='https://erpsystem.testdomain100.online/'
 // export const environment = {
@@ -21,14 +21,14 @@ export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
 //alkoot
 
 
-// export const baseUrl='https://alkoot-restaurant.com/'
-//  export const environment = {
-//    production: true,
-//    pusher: {
-//      key: '77f608d73899bd256cfa',
-//      cluster: 'mt1',
-//    }
-//  };
+export const baseUrl='https://erpmain.alkoot-restaurant.com/'
+ export const environment = {
+   production: true,
+   pusher: {
+     key: '77f608d73899bd256cfa',
+     cluster: 'mt1',
+   }
+ };
 
 
 
@@ -37,16 +37,16 @@ export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
 
 // export const baseUrl='https://productowner.testdomain100.online/'
 // export const baseUrl='https://erpsystem.testdomain100.online/'
-export const baseUrl='https://erpsystem.testdomain100.online/'
+// export const baseUrl='https://erpsystem.testdomain100.online/'
 
 
- export const environment = {
-   production: true,
-   pusher: {
-     key: 'cfd52a74b92f9e278f2d',
-     cluster: 'mt1',
-   }
- };
+//  export const environment = {
+//    production: true,
+//    pusher: {
+//      key: 'cfd52a74b92f9e278f2d',
+//      cluster: 'mt1',
+//    }
+//  };
 
 export const environment2 = {
   production: true,
