bc5f5df8d89f78af910ab345f00e5366863f4f1e
eng-mohamed-gomah <mohamed_agamy@mkodekw.net>
2026-03-02 14:30:17 +0200

    fix-reverb

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,10 +2,21 @@
 //  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
   export const baseUrl ='http://127.0.0.1:8000/'
-   export const baseUrl2 ='https://erpsystem.testdomain100.online/api'
+   export const baseUrl2 ='http://127.0.0.1:8000/api'
 
 
-export const environment = {
+// export const environment = {
+//   production: false,
+//   reverb: {
+//     key: '77f608d73899bd256cfa',
+//     wsHost: '127.0.0.1',
+//     wsPort: 8080,
+//     wssPort: 8080,
+//     forceTLS: false,
+//     enabledTransports: ['ws', 'wss'],
+//   },
+// };
+export const environment2 = {
   production: false,
   reverb: {
     key: '77f608d73899bd256cfa',
@@ -16,14 +27,22 @@ export const environment = {
     enabledTransports: ['ws', 'wss'],
   },
 };
-export const environment2 = {
+
+export const environment = {
   production: false,
+  pusher: {
+    key: '45700f4c3cc882528180',
+    cluster: 'eu',
+    wsHost: 'http://localhost:8000',
+    wsPort: 6001,
+    forceTLS: false,
+  },
   reverb: {
-    key: '77f608d73899bd256cfa',
+    key: '45700f4c3cc882528180',
     wsHost: '127.0.0.1',
     wsPort: 8080,
     wssPort: 8080,
     forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'],
   },
 };
