0ad3388031ca793b22f0236a553321c1829f4f8f
eng-mohamed-gomah <mohamed_agamy@mkodekw.net>
2026-03-12 10:05:40 +0200

    socket

--- src/app/services/sync-offline.service.ts
+++ src/app/services/sync-offline.service.ts
@@ -21,4 +21,14 @@ export class SyncOfflineService {
 
     return this.http.post(this.syncUrl, {}, { headers });
   }
+
+  triggerActivityLogSync(): Observable<any> {
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(`${baseUrl}api/syncoffline/activity-logs/push`, {}, { headers });
+  }
 }
