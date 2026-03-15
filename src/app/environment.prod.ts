cfdc71ac7a77fe81780a86f140156f07710926fb
eng-mohamed-gomah <mohamed_agamy@mkodekw.net>
2026-03-15 13:37:21 +0200

    env

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -3,9 +3,9 @@
 
 // export const baseUrl2 ='https://erp-cashier.testdomain100.online/api'
 
-export const baseUrl ='http://192.168.100.11:8000/'
+export const baseUrl ='http://192.168.20.178/'
 // export const baseUrl2 ='https://productowner.testdomain100.online/api'
-export const baseUrl2 ='http://192.168.100.11:8000/api'
+export const baseUrl2 ='http://192.168.20.178/api'
 
 // export const baseUrl2 ='https://erpmain.alkoot-restaurant.com/api'
 
@@ -17,7 +17,7 @@ export const environment = {
     key: 'cfd52a74b92f9e278f2d',
     cluster: 'mt1',
   },
-  wsUrl: 'ws://192.168.100.11:8081'
+  wsUrl: 'ws://192.168.20.178:8081'
 };
 
 
