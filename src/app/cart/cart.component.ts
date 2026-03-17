a8319da58c2ceefae903cf6872b96f663daca1c6
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2026-03-17 14:38:04 +0200

    delivery

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -781,7 +781,14 @@ export class CartComponent {
           this.message = res?.message || 'تم تغيير نوع الطلب بنجاح';
           this.status_order = true;
           setTimeout(() => { this.message = ''; }, 3000);
-          this.fetchOrderDetails();
+          // this.fetchOrderDetails();
+          setTimeout(() => {
+            const id = this.currentOrderForTypeChange.order_details.order_id;
+            this.router.navigate(
+              ['/order-details', id],
+              { queryParams: { refresh: 'true' } }
+            );
+          }, 700);
           this.currentOrderForTypeChange = null;
           this.selectedNewOrderType = '';
           this.selectedTableIdForTypeChange = '';
