e1afabd50d016da96129d67501c1d7f161f22630
eng-mohamed-gomah <mohamed_agamy@mkodekw.net>
2026-03-26 14:48:55 +0200

    creator and closer order

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,8 +2,8 @@
 //  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
 //  export const baseUrl ='https://erp-cashier.testdomain100.online/'
-export const baseUrl ='http://192.168.11.228:8000/'
-export const baseUrl2 ='http://192.168.11.228:8000/api'
+export const baseUrl ='http://localhost:8000/'
+export const baseUrl2 ='http://localhost:8000/api'
 
 //test
 export const environment = {
@@ -12,7 +12,7 @@ export const environment = {
     key: 'cfd52a74b92f9e278f2d',
     cluster: 'mt1',
   },
-  wsUrl: 'ws://127.0.0.1:8081'
+  wsUrl: 'ws://localhost:8081'
 };
 
 // //localhost
