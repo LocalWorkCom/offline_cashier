8b82d604d6232a09441f0d4b73932fd4a0829068
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2026-04-09 10:42:53 +0200

    pos front

--- src/app/payment-device-management/payment-device-delete-modal.component.ts
+++ src/app/payment-device-management/payment-device-delete-modal.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { baseUrl2 } from '../environment';
 
 @Component({
   selector: 'app-payment-device-delete-modal',
@@ -10,11 +12,23 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PaymentDeviceDeleteModalComponent {
   @Input() deviceName = '';
+  @Input() deviceId?: number;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal, private httpClient: HttpClient) {}
 
   confirmDelete(): void {
-    this.activeModal.close(true);
+    if (!this.deviceId) {
+      return;
+    }
+
+    this.httpClient.post(`${baseUrl2}/payment-device/delete/${this.deviceId}`, {}).subscribe({
+      next: () => {
+        this.activeModal.close(true);
+      },
+      error: () => {
+        this.activeModal.dismiss();
+      }
+    });
   }
 }
 
