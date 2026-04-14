1fa54d10bf7d83862953d2ef976341e7f559c107
eng-mohamed-gomah <mohamed_agamy@mkodekw.net>
2026-04-14 10:19:25 +0200

    socket

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -19,7 +19,7 @@ export const baseUrl2 ='http://192.168.11.101:8000/api'
 // export const baseUrl='https://erpsystem.testdomain100.online/'
 export const environment = {
   production: true,
-  broadcast_type: 'socket', // 'pusher' or 'socket'
+  broadcast_type: 'pusher', // 'pusher' or 'socket'
   pusher: {
     key: 'cfd52a74b92f9e278f2d',
     cluster: 'mt1',
