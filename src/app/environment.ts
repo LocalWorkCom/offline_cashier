d467320ce4d30ce5e60ca97633e81ea3a8584065
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2026-05-17 11:33:40 +0300

    s

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,8 +2,8 @@
 //  export const baseUrlForWebSocket ='https://erpfortest.testdomain100.online/'
 
 //  export const baseUrl ='https://erp-cashier.testdomain100.online/'
-export const baseUrl ='http://127.0.0.1:8000/'
-export const baseUrl2 ='http://127.0.0.1:8000/api'
+export const baseUrl ='https://productowner.testdomain100.online/'
+export const baseUrl2 ='https://productowner.testdomain100.online/api'
 
 //test
 export const environment = {
